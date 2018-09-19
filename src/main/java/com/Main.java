0b221e5f22721521706f065c21c422988428af01
Jacob <jacobisaac1997@gmail.com>
2018-09-19 12:20:14 -0400

    impl: game starts on 'c'. Can play through player's turn

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -5,29 +5,88 @@ import com.BlackjackGame;
 
 public class Main {
 	
-	public static int PLAYER = 0;
-	public static int DEALER = 1;
-	
 	public static void main(String[] args)
 	{
 		BlackjackGame game = new BlackjackGame();
 		
 		System.out.println("Starting the game!");
 		
-		Scanner reader = new Scanner(System.in);  // Reading from System.in
+		Scanner reader = new Scanner(System.in);
 		char c;
 		while(true)
 		{
-			System.out.println("Enter either f or c: ");
-			c = reader.next().charAt(0); // Scans the next token of the input as an int.
+			System.out.print("Enter either f or c: ");
+			c = Character.toLowerCase(reader.next().charAt(0));
 			if (c == 'f' || c == 'c')
 			{
 				break;
 			}
-			System.out.println("That was not one of the numbers requested!");
+			System.out.println("That was not one of the letters requested!");
 		}
-		//once finished
-		reader.close();
-		System.out.println(DEALER);
+		
+		switch(c)
+		{
+			case 'c':
+			{
+				game.player.hit(game.deck);
+				game.player.hit(game.deck);
+				game.dealer.hit(game.deck);
+				game.dealer.hit(game.deck);
+				
+				if (game.dealer.hasBlackjack() || game.player.hasBlackjack())
+				{
+					game.playOut();
+				}
+				
+				while(game.gameOver == false)
+				{
+					System.out.println("The player currently has the following cards:");
+					System.out.println(game.player.hand.toString().substring(1, game.player.hand.toString().length() - 1));
+					
+					System.out.println("The dealer's first card is:");
+					System.out.println(game.dealer.hand.get(0));
+					
+					while (true)
+					{
+						System.out.print("Enter either h to hit or s to stand and end your turn: ");
+						
+						char playerChoice;
+						
+						playerChoice = Character.toLowerCase(reader.next().charAt(0));
+						if (playerChoice == 'h')
+						{
+							game.player.hit(game.deck);
+							System.out.println("You hit");
+							//System.out.println(game.player.hand.toString().substring(1, game.player.hand.toString().length() - 1));
+							if (game.player.hasBusted())
+							{
+								game.playOut();
+								break;
+							}
+						}
+						else if (playerChoice == 's')
+						{
+							System.out.println("You stood. Your cards are: ");
+							System.out.println(game.player.hand.toString().substring(1, game.player.hand.toString().length() - 1));
+							break;
+						}
+						else
+						{
+							System.out.println("That was not one of the values requested!");
+						}
+					}
+					
+					reader.close();
+					
+					game.playOut();
+				}
+				
+				break;
+			}
+			case 'f':
+			{
+				break;
+			}
+		}
 	}
 }
