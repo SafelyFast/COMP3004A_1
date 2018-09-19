e660380c095b30e9612578626e281b422beef0ed
Jacob <jacobisaac1997@gmail.com>
2018-09-19 19:48:31 -0400

    impl: Finished off game logic. Works for file and console input

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,9 +1,15 @@
 package com;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import com.BlackjackGame;
 
 public class Main {
+	
+	static int DEALER = 1;
 	
 	public static void main(String[] args)
 	{
@@ -35,6 +41,8 @@ public class Main {
 				
 				if (game.dealer.hasBlackjack() || game.player.hasBlackjack())
 				{
+					System.out.println("Somebody got a blackjack!");
+					System.out.println((game.dealer.hasBlackjack() ? "Dealer" : "Player") + " has blackjack!");
 					game.playOut();
 				}
 				
@@ -57,9 +65,10 @@ public class Main {
 						{
 							game.player.hit(game.deck);
 							System.out.println("You hit");
-							//System.out.println(game.player.hand.toString().substring(1, game.player.hand.toString().length() - 1));
+							System.out.println(game.player.hand.toString().substring(1, game.player.hand.toString().length() - 1));
 							if (game.player.hasBusted())
 							{
+								System.out.println("You busted!");
 								game.playOut();
 								break;
 							}
@@ -72,19 +81,151 @@ public class Main {
 						}
 						else
 						{
-							System.out.println("That was not one of the values requested!");
+							System.out.println("That was not one of the letters requested!");
+						}
+					}
+					
+					if (game.gameOver == false)
+					{
+						System.out.println("The dealer reveals his cards. They are: ");
+						System.out.println(game.dealer.hand.toString().substring(1, game.dealer.hand.toString().length() - 1));
+						
+						while (true)
+						{
+							if (game.dealer.mustHit() || game.dealer.hasSoft17())
+							{
+								System.out.println("Dealer is hitting");
+								game.dealer.hit(game.deck);
+								System.out.println("Dealer received a " + game.dealer.hand.get(game.dealer.hand.size() - 1));
+								if (game.dealer.hasBusted())
+								{
+									System.out.println("Dealer busts!");
+									game.playOut();
+									break;
+								}
+							}
+							else
+							{
+								System.out.println("Dealer stands. His hand is: ");
+								System.out.println(game.dealer.hand.toString().substring(1, game.dealer.hand.toString().length() - 1));
+								break;
+							}
 						}
 					}
 					
 					reader.close();
 					
 					game.playOut();
+					
+					System.out.println((game.victor == DEALER ? "Dealer" : "Player") + " wins!");
 				}
 				
 				break;
 			}
 			case 'f':
 			{
+				String file = null;
+				Scanner inputFile = null;
+				List<String> deck = new ArrayList<String>();
+				String drawnCard = null;
+				
+				while (true)
+				{
+					System.out.print("Enter the name of the file (and extension) that you would like to open. It MUST be in the src/main/resources directory: ");
+					file = reader.next();
+					try 
+					{
+						inputFile = new Scanner(new File("./src/main/resources/" + file));
+						while (inputFile.hasNextLine())
+						{
+							deck.add(inputFile.nextLine());
+						}
+						break;
+					}
+					catch(FileNotFoundException e)
+					{
+						System.out.println("The file you specified does not exist");
+					}
+				}
+				
+				inputFile.close();
+				
+				game.player.hit(deck);
+				game.player.hit(deck);
+				game.dealer.hit(deck);
+				game.dealer.hit(deck);
+				
+				System.out.println("The player currently has the following cards:");
+				System.out.println(game.player.hand.toString().substring(1, game.player.hand.toString().length() - 1));
+				
+				System.out.println("The dealer's first card is:");
+				System.out.println(game.dealer.hand.get(0));
+				
+				//MAKE PLAYER TURN AND DEALER TURN
+				
+				if (game.dealer.hasBlackjack() || game.player.hasBlackjack())
+				{
+					System.out.println("Somebody got a blackjack!");
+					System.out.println((game.dealer.hasBlackjack() ? "Dealer" : "Player") + " has blackjack!");
+					game.playOut();
+				}
+				
+				while(true)
+				{
+					if (deck.isEmpty())
+					{
+						break;
+					}
+					drawnCard = deck.remove(0);
+					if (drawnCard.equals("H"))
+					{
+						game.player.hit(deck);
+						System.out.println("You hit");
+						System.out.println(game.player.hand.toString().substring(1, game.player.hand.toString().length() - 1));
+						if (game.player.hasBusted())
+						{
+							System.out.println("You busted!");
+							game.playOut();
+							break;
+						}
+					}
+					else if (drawnCard.equals("S"))
+					{
+						System.out.println("You stood. Your cards are: ");
+						System.out.println(game.player.hand.toString().substring(1, game.player.hand.toString().length() - 1));
+						break;
+					}
+				}
+				
+				if (game.gameOver == false)
+				{
+					System.out.println("The dealer reveals his cards. They are: ");
+					System.out.println(game.dealer.hand.toString().substring(1, game.dealer.hand.toString().length() - 1));
+					
+					while (true)
+					{
+						if (deck.isEmpty() == true)
+						{
+							System.out.println("Dealer stands. His hand is: ");
+							System.out.println(game.dealer.hand.toString().substring(1, game.dealer.hand.toString().length() - 1));
+							break;
+						}
+						System.out.println("Dealer is hitting");
+						game.dealer.hit(deck);
+						System.out.println("Dealer received a " + game.dealer.hand.get(game.dealer.hand.size() - 1));
+						if (game.dealer.hasBusted())
+						{
+							System.out.println("Dealer busts!");
+							game.playOut();
+							break;
+						}
+					}
+				}
+				
+				game.playOut();
+				
+				System.out.println((game.victor == DEALER ? "Dealer" : "Player") + " wins!");
+				
 				break;
 			}
 		}
