65ff3fe3f2bfcadb5c9af009115dc4ce3d7eb293
Jacob <jacobisaac1997@gmail.com>
2018-09-19 11:22:02 -0400

    new: Added main file. Moved main func to main file

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,30 @@
+package com;
+
+import java.util.Scanner;
+import com.BlackjackGame;
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		BlackjackGame game = new BlackjackGame();
+		
+		System.out.println("Starting the game!");
+		
+		Scanner reader = new Scanner(System.in);  // Reading from System.in
+		char c;
+		while(true)
+		{
+			System.out.println("Enter either f or c: ");
+			c = reader.next().charAt(0); // Scans the next token of the input as an int.
+			if (c == 'f' || c == 'c')
+			{
+				break;
+			}
+			System.out.println("That was not one of the numbers requested!");
+		}
+		//once finished
+		reader.close();
+		System.out.println(game.deck.size());
+	}
+}
